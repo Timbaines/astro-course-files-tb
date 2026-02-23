f1e8418f7818162177faed93b584dfa6522c4bc8
Timbaines <timbaines@timbaines.com>
2026-02-23 16:51:06 -0500

    Add React integration and update TypeScript configuration
    Integrate React with Astro by enabling the `@astrojs/react` package and updating dependencies. Adjust TypeScript settings to support React JSX syntax and improve path alias formatting for readability.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    env: {
-        schema: {
-            // ...
-        }
-    }
+  env: {
+      schema: {
+          // ...
+      }
+  },
+
+  integrations: [react()]
 });
